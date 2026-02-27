56340def6a676beeb5d71e93c6bf45a25bf5aa17
manju <manjunathdesai861@gmail.com>
2026-02-27 20:44:40 +0530

    commit

---
+++ source/repos/InheritanceExample/InheritanceExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string CaraName { get; set; }
+
+        public Car(int id,string name)
+        {
+            CarId = id;
+            CaraName = name;
+        }
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine($"Carid :{CarId},  Carname:{CaraName}");
+        }
+    }
+    public class NewCar:Car
+    {
+        public double CarAge { get; set; }
+
+        public NewCar(int id, string name, double age):base(id,name)
+        {
+            CarAge = age;
+        }
+        public override void ShowDetails()
+        {
+            Console.WriteLine($"Car id:{CarId}, CarName:{CaraName}, Car age :{CarAge}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewCar newCar = new NewCar(1,"Swift",0.1);
+            newCar.ShowDetails();
+        }
+    }
+}
